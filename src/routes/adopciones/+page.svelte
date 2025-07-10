183b96ebf25f0f0bc01184082b7baf19ebb480cf
TDkRecords <tdksongs@gmail.com>
2025-07-10 14:55:18 -0500

    Añadiendo petfinder

--- src/routes/adopciones/+page.svelte
+++ src/routes/adopciones/+page.svelte
@@ -1,8 +1,22 @@
-<section class="bg-light py-5">
+<script>
+    // No necesitas lógica adicional aquí, el widget se encarga
+</script>
+
+<section class="bg-light py-4">
     <div class="container">
-        <h1 class="text-success text-center fw-bold mb-4">
-            <i class="fas fa-paw me-2"></i>Adopciones responsables
-        </h1>
-        <p class="text-center fs-5 mb-5">Pronto Mascotas</p>
+        <div class="d-flex justify-content-center">
+            <pet-scroller
+                type="[]"
+                age="[]"
+                limit="24"
+                status="adoptable"
+                petlisttitle=""
+                organization="NJ937,AZ663,IL947,KY527,WV217,PA1121,FL1568,ON591,FL1625,FL1579,FL1706,VA818,CA2748"
+                apibase="https://api.petfinder.com"
+                petfinderurl="https://www.petfinder.com"
+                accesstoken="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJtd1NmUDQ1SEpPckFSS0RkVGM1M3JGSjNHVTJIdnk0SUxVSGR5Y3NnNGpURzRCVHIzdCIsImp0aSI6InRTaTVIZ0ZHeXgwZWNaa3R4TjI1Sm9XTGNBTGc2WWNNcFE4aHhYaU4iLCJpYXQiOjE3NTIxNzY5ODAsIm5iZiI6MTc1MjE3Njk4MCwiZXhwIjo0OTA3ODUwNTgwLCJzdWIiOiIxOTU3MzM3NyIsInNjb3BlcyI6W119.avPLFOve6L8r3EqSbWDlXpnevESpUnnGfOO20zsQJzC0csyHnPjJD7jDoBktSJBSPLODdiIdftzX9ms7PJZKf2dbHo4c5y7LoRociLtWMG7aWVwRd1nOYGto3OLjTvev1hrpDTmujBYWuA9zWGN6lclbPcgkvcMWZ3CqoCkKPPYQJMvGjYj2d3FvNhTql9yoq3M53u--_8fhSXCZBW62tzb-yUe0qZs5pX5ftSqXSTs7mIlYcxFyYyqEnBmbLhNkCqW1d_RLnaPWos1NkA8W_gpwrnrMxwtggiavLwND-kPRe0T76ljdGBZ9PO1ZmIJoXILc8M_0wrpW-LpIkENhNA"
+            >
+            </pet-scroller>
+        </div>
     </div>
 </section>
